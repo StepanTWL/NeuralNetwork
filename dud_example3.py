2a8887a64ba3354f3b6310cc3cec0b05d233b87c
Stepan D <h3xp3ctb@gmail.com>
2023-07-20 17:58:09 +0300

    20.07.2023

--- dud_example3.py
+++ dud_example3.py
@@ -26,5 +26,19 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img_filter = cv2.bilateralFilter(gray, 11, 15, 15) # диаметр (насколько много пикселей будет охвачено), цветовое пространство (насколько много пикселей с примерно одинаковым цветом будет смешано), координатное пространство (тоже что и 2 только одинаковые по координатам)
 img_edges = cv2.Canny(img_filter, 30, 200) # пороги
 
-pl.imshow(cv2.cvtColor(img_edges, cv2.COLOR_BGR2RGB))
-pl.show()
+cont = cv2.findContours(img_edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # ищет контуры (режим (в не иерархичном порядке), метод нахождения контуров (позволяет найти начальные и конечные точки без промежуточных))
+cont = imutils.grab_contours(cont) # выдает контуры в удобном формате
+cont = sorted(cont, key=cv2.contourArea, reverse=True)[:20] # отсортирует по квардатам
+
+pos = None
+for c in cont:
+    approx = cv2.approxPolyDP(c, 10, True) # большее число указывает что фигура ближе к квадрату, что начальная и конечная точка фигуры находится в одном месте (закрытая форма)
+    if len(approx) == 4: # фигура состоит из 4 элементов (углов)
+        pos = approx
+        break
+
+mask = np.zeros(gray.shape, np.uint8) # маска
+
+
+#pl.imshow(cv2.cvtColor(img_edges, cv2.COLOR_BGR2RGB))
+#pl.show()
