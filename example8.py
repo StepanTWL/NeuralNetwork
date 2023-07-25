fabbb6a066a1d45b32fc2c6e6883aadb8d1b1b93
Stepan D <h3xp3ctb@gmail.com>
2023-07-25 17:59:46 +0300

    25.07.2023 work

--- example8.py
+++ example8.py
@@ -7,13 +7,12 @@ import re
 
 from tensorflow.keras.layers import Dense, SimpleRNN, Input
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.text import Tokenizer
 
 with open('train_data_true.txt', 'r', encoding='utf-8') as f:
     text = f.read()
     text = text.replace('\ufeff', '') # убираем первый невидимый символ
-    text = re.sub(r'[^А-я]', '',  text) # заменяет все кроме кириллицы на пустые символы
+    text = re.sub(r'[^А-я ]', '',  text) # заменяет все кроме кириллицы на пустые символы
 
 num_characters = 34 # 33 буквы + пробел
 tokenirez = Tokenizer(num_words=num_characters, char_level=True) # токенизация текста на уровне символов
@@ -37,10 +36,21 @@ model.summary()
 
 model.compile(loss='categorical_crossentropy', metrics=['accuracy'],  optimizer='adam') # потери - категориальная кросс ентропия, метрикика - точность,  оптимизатор для градиентного спуска - адам
 
-history = model.fit(X, Y, batch_size=32, epochs=100) # тренеровка модели
+history = model.fit(X, Y, batch_size=32, epochs=200) # тренеровка модели
 
 def buildPhrase(inp_str, str_len=50):
     for i in range(str_len):
         x = []
         for j in range(i, i + inp_chars):
-            x.append(())
+            x.append(tokenirez.texts_to_matrix(inp_str[j])) # преобразовываеим текст в матрицу токенов (массива OHE)
+
+        x = np.array(x)
+        inp = x.reshape(1, inp_chars, num_characters) # формирует начальные символы по которым будет строиться прогноз
+        pred = model.predict(inp) # на вход нейронной сети (предсказание 7 символа)
+        d = tokenirez.index_word[pred.argmax(axis=1)[0]] # находим индекс наибольшего значения и подаем на колекцию (словарь)
+
+        inp_str += d
+    return inp_str
+
+res = buildPhrase('привет')
+print(res)
---
+++ example9.py
@@ -0,0 +1,65 @@
+##################### Прогнозирование слов #####################
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import numpy as np
+
+from tensorflow.keras.layers import Dense, SimpleRNN, Input, Embedding
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.text import Tokenizer, text_to_word_sequence
+from tensorflow.keras.utils import to_categorical
+
+with open('text', 'r', encoding='utf-8') as f:
+    texts = f.read()
+    texts = texts.replace('\ufeff', '')
+
+maxWordsCount = 1000
+tokenizer = Tokenizer(num_words=maxWordsCount, filters='!–"—#$%&amp;()*+,-./:;<=>?@[\\]^_`{|}~\t\n\r«»',
+                      lower=True, split=' ', char_level=False) # разбиваем текст на слова (первые 1000 часто встречающихся слов)
+tokenizer.fit_on_texts([texts]) # преобразовываем текс
+
+dist = list(tokenizer.word_counts.items())
+print(dist[:10])
+
+data = tokenizer.texts_to_sequences([texts]) # преобразование в OHE вектор
+#res = to_categorical(data[0], num_classes=maxWordsCount) # формируем 3х мерный тензор
+#print(res.shape)
+res = np.array(data[0])
+
+inp_words = 3
+n = res.shape[0] - inp_words
+
+X = np.array([res[i:i + inp_words] for i in range(n)]) # формируем 3х мерный тензоры
+# Y = res[inp_words:]
+Y = to_categorical(res[inp_words:], num_classes=maxWordsCount)
+
+# Рекурентная нейронная сеть
+model = Sequential()
+model.add(Embedding(maxWordsCount, 256, input_length = inp_words)) # при тренировке в рекуррентные модели keras подается вся последовательность
+model.add(SimpleRNN(128, activation='tanh')) # рекуррентный слой на 128 нейронов (функция кативации - гипербалический тангенс)
+model.add(Dense(maxWordsCount, activation='softmax')) # полносвязный слой (функция кативации - softmax)
+model.summary()
+
+model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+history = model.fit(X, Y, batch_size=32, epochs=50)
+
+def buildPhrase(texts, str_len=20):
+    res = texts
+    data = tokenizer.texts_to_sequences([texts])[0]
+    for i in range(str_len):
+        #x = to_categorical(data[i:i + inp_words], num_classes=maxWordsCount)
+        #inp = x.reshape(1, inp_words, maxWordsCount)
+        x = data[i:i+inp_words]
+        inp = np.expand_dims(x, axis=0)
+
+        pred = model.predict(inp) # на вход нейронной сети (предсказание 3 слова)
+        indx = pred.argmax(axis=1)[0] # индекатор макисмальноезначения
+        data.append(indx)
+
+        res += " " + tokenizer.index_word[indx]
+
+    return res
+
+
+res = buildPhrase("позитив добавляет годы")
+print(res)
